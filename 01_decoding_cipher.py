99f76933f489cfee644cadaa59641f3e8fa021ba
Lucas Tymen <lucas.tymen@gmail.com>
2023-04-28 17:33:26 +0200

    coded_correspondance_2

--- 01_decoding_cipher.py
+++ 01_decoding_cipher.py
@@ -1,17 +1,15 @@
 letters = "abcdefghijklmnopqrstuvwxyz"
-
 message = "xuo jxuhu! jxyi yi qd unqcfbu ev q squiqh syfxuh. muhu oek qrbu je tusetu yj? y xefu ie! iudt cu q cuiiqwu rqsa myjx jxu iqcu evviuj!"
+traduction = ""
 
-translated_message = ""
+for char in message:
+    if char in letters:
+        char_value = letters.find(char)
+        traduction += letters[(char_value + 10) % 26]
+    else:
+        traduction += char
 
-for character in message:
-  if character in letters:
-    character_value = letters.find(character)
-    translated_message += letters[(character_value + 10) % 26]
-  else:
-    translated_message += character
-
-print(translated_message)
+print(traduction)
 
 
 ############################ Solution ###########################
---
+++ 02_encoding_cypher.py
@@ -0,0 +1,12 @@
+letters = "abcdefghijklmnopqrstuvwxyz"
+message = "ok, i've decoded your message. now let's go with next steps on our journey to python."
+traduction = ""
+
+for char in message:
+    if char in letters:
+        char_value = letters.find(char)
+        traduction += letters[(char_value - 10) * 26]
+    else:
+        traduction += char
+
+print(traduction)
