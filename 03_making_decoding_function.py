f50d8f2cf845d23f4f0d7c615addd77a0c1f73b7
Lucas Tymen <lucas.tymen@gmail.com>
2023-04-28 17:44:12 +0200

    coded_correspondance_3

---
+++ 03_making_decoding_function.py
@@ -0,0 +1,20 @@
+letters = "abcdefghijklmnopqrstuvwxyz"
+message = "xuo jxuhu! jxyi yi qd unqcfbu ev q squiqh syfxuh. muhu oek qrbu je tusetu yj? y xefu ie! iudt cu q cuiiqwu rqsa myjx jxu iqcu evviuj!"
+traduction = ""
+
+def caesar_decode(message, offset):
+    for char in message:
+        if char in letters:
+            char_value = letters.find(char)
+            traduction += letters[(char_value - 10) % 26]
+    else:
+        traduction += char
+
+
+def caesar_encode(message, offset):
+    for char in message:
+        if char in letters:
+            char_value = letters.find(char)
+            traduction += letters[(char_value + 10) % 26]
+    else:
+        traduction += char
