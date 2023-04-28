9239e8744123afb4eb0e9549c49207a9a47a0b6b
Lucas Tymen <lucas.tymen@gmail.com>
2023-04-28 17:21:57 +0200

    coded_correspondance_1

--- cipher.py
+++ cipher.py
@@ -1,3 +1,20 @@
+letters = "abcdefghijklmnopqrstuvwxyz"
+
+message = "xuo jxuhu! jxyi yi qd unqcfbu ev q squiqh syfxuh. muhu oek qrbu je tusetu yj? y xefu ie! iudt cu q cuiiqwu rqsa myjx jxu iqcu evviuj!"
+
+translated_message = ""
+
+for character in message:
+  if character in letters:
+    character_value = letters.find(character)
+    translated_message += letters[(character_value + 10) % 26]
+  else:
+    translated_message += character
+
+print(translated_message)
+
+
+############################ Solution ###########################
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 message = "xuo jxuhu! jxyi yi qd unqcfbu ev q squiqh syfxuh. muhu oek qrbu je tusetu yj? y xefu ie! iudt cu q cuiiqwu rqsa myjx jxu iqcu evviuj!"
