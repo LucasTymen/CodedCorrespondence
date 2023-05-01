5dc602f6851d8d0d3cb468ab863ab587af43805e
Lucas Tymen <lucas.tymen@gmail.com>
2023-05-01 23:19:21 +0200

    coded_correspondance_3

---
+++ coded-correspondence/step3_functions.py
@@ -0,0 +1,21 @@
+letters = "abcdefghijklmnopqrstuvwxyz"
+message = "xuo jxuhu! jxyi yi qd unqcfbu ev q squiqh syfxuh. muhu oek qrbu je tusetu yj? y xefu ie! iudt cu q cuiiqwu rqsa myjx jxu iqcu evviuj!"
+traduction = ""
+
+def decode_cipher(letters, message):
+    for char in message:
+        if char in letters:
+            char_value = letters.find(char)
+            traduction += letters[(char_value + 10) % 26]
+        else:
+            traduction += char
+
+def encode_cipher(letters, message):
+    for char in message:
+        if char in letters:
+            char_value = letters.find(char)
+            traduction += letters[(char_value - 10) % 26]
+        else:
+            traduction += char
+
+    print(traduction)
