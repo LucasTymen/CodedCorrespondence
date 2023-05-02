6ccd8cde4dd415ee408177b31d783dc56c9a4c89
Lucas Tymen <lucas.tymen@gmail.com>
2023-05-02 16:04:26 +0200

    coded_correspondance_5

---
+++ coded-correspondence/step5_brute_force.py
@@ -0,0 +1,5 @@
+brute_force_message = "vhfinmxkl atox kxgwxkxw tee hy maxlx hew vbiaxkl tl hulhexmx. px'ee atox mh kxteer lmxi ni hnk ztfx by px ptgm mh dxxi hnk fxlltzxl ltyx."
+
+for i in range(1, 26):
+  print("Offset: {}".format(i))
+  print("\t {}".format(caesar_decode(brute_force_message, i)))
