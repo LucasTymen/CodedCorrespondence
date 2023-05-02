4d19c051e188060c423a7fb67397bf2b2501908f
Lucas Tymen <lucas.tymen@gmail.com>
2023-05-02 10:37:33 +0200

    coded_correspondance_3

--- coded-correspondence/step3_functions.py
+++ coded-correspondence/step3_functions.py
@@ -1,22 +1,37 @@
-letters = "abcdefghijklmnopqrstuvwxyz"
-message = ""
-traduction = ""
-char_value = 0
+alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-def decode_cipher(letters, message):
-    for char in message:
-        if char in letters:
-            char_value = letters.find(char)
-            decode_traduction += letters[(char_value + 10) % 26]
-        else:
-            traduction += char
+def caesar_decode(message, offset):
+  decoded_message = ""
 
-def encode_cipher(letters, message):
-    for char in message:
-        if char in letters:
-            char_value = letters.find(char)
-            traduction += letters[(char_value - 10) % 26]
-        else:
-            encode_traduction += char
+  for character in message:
+    if character in alphabet:
+      character_value = alphabet.find(character)
+      decoded_message += alphabet[(character_value + offset) % 26]
+    else:
+      decoded_message += character
 
-    print(encode_traduction)
+  return decoded_message
+
+def caesar_encode(message, offset):
+  encoded_message = ""
+
+  for character in message:
+    if character in alphabet:
+      character_value = alphabet.find(character)
+      encoded_message += alphabet[(character_value - offset) % 26]
+    else:
+      encoded_message += character
+
+  return encoded_message
+
+
+
+message_one = "jxu evviuj veh jxu iusedt cuiiqwu yi vekhjuud."
+
+# Now we'll print the output of `caeser_decode()` for the first message with an offset of 10
+print(caesar_decode(message_one, 10))
+
+# Now we know what offset to use for the second message, so we use that to solve.
+message_two = "bqdradyuzs ygxfubxq omqemd oubtqde fa oapq kagd yqeemsqe ue qhqz yadq eqogdq!"
+
+print(caesar_decode(message_two, 14))
